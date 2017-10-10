35ea60ffceac90ec007930c94faaa959532fc8d0
Егор Зотов < >
2017-10-10 11:38:53 +0300

    file write added

--- api/login.php
+++ api/login.php
@@ -1,3 +1,18 @@
 <?
-
-print_r($_POST);
+if($_POST['action'] == "save"){
+	$file = dirname(dirname(__FILE__))."\secret-files\userdata.json";
+	$file_content = file_get_contents($file);
+	$data = (!empty($file_content)?json_decode($file_content):array());
+	unset($_POST['action']);
+	array_push($data,$_POST);
+	$result = file_put_contents($file, json_encode($data));
+	$respond = array(); 
+	if ($result){
+		$respond['status'] = 'success';
+	} else {
+		$respond['status'] = 'error';
+	}
+	echo json_encode($respond); 
+}
+ 
+// закрываем
